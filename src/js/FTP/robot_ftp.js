9c5b556b81158f84e57c6d037d7336d7fc2871f0
anton feldmann <anton.feldmann@gmail.com>
2018-05-18 17:00:13 +0900

    ftp config file

--- src/js/FTP/robot_ftp.js
+++ src/js/FTP/robot_ftp.js
@@ -1,28 +1,42 @@
 'use strict';
 
-require('dotenv').config();
-var tempfile = require('tempfile');
+var config = require('config');
+
+console.log(config);
+
+let with_ftp = config.has('ftp')? config.get('ftp') : false;
+let robots;
+
+if (with_ftp){
+    robots = config.has('robots')? config.get('robots') : [];
+}
+
+console.info(robots);
+
+/*if (config.has('optionalFeature.detail')) {
+  var detail = config.get('optionalFeature.detail');
+  //...
+}*/
+
+/*
 var Client = require('ftp');
 var fs = require('fs');
 
 var c = new Client();
-c.on('ready', function() {
-    /*c.list(function(err, list) {
-	if (err) throw err;
-	console.dir(list);
-    });*/
-    
+
+c.on('ready', function() {    
     c.get('errall.ls', function(err, stream) {
 	if (err) throw err;
 	stream.once('close', function() { c.end(); });
-	stream.pipe(fs.createWriteStream('local.txt'));
+	stream.pipe(fs.createWriteStream('test.txt'));
 
     });
 });
 
 c.connect({
-    host: process.env.FANUC_HOST,
+    host: process.env.ROBOT_HOST,
     port: 21,
-    user: process.env.FANUC_USER,
-    password: process.env.FANUC_PASSWD
-    });
+    user: process.env.ROBOT_USER,
+    password: process.env.ROBOT_PASSWD
+});
+*/
